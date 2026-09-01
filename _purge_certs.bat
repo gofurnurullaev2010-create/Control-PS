3d8e66ac505a19126d12db3357d9109bbd35e121
gofurnurullaev2010-create <gofurnurullaev2010-create@users.noreply.github.com>
2026-09-01 22:58:37 +0500

    Avtomatik yangilanish: 2026-09-01_22:58

--- _purge_certs.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set "PATH=C:\Program Files\Git\cmd;%PATH%"
-cd /d "D:\Control PS"
-set FILTER_BRANCH_SQUELCH_WARNING=1
-git filter-branch -f --index-filter "git rm -r --cached --ignore-unmatch vidaa" --prune-empty -- --all
-echo ---- TARIXDA QOLDIMI? ----
-git log --all --name-only --pretty=format: -- vidaa
-echo ---- TUGADI ----
